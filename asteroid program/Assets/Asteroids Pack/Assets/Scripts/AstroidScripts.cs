17f703756ac9fcc4413d14f0333a18be4a16d77f
Hwoods52 <109534608+Hwoods52@users.noreply.github.com>
2022-07-18 11:58:19 -0700

    .

---
+++ asteroid program/Assets/Asteroids Pack/Assets/Scripts/AstroidScripts.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class AstroidScripts : MonoBehaviour
+{
+    public TextMeshProUGUI ScoreText;
+    public float speed;
+    private Rigidbody asteroidRb;
+    public GameObject ship;
+    public int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        asteroidRb = GetComponent<Rigidbody>();
+        ship = GameObject.Find("ship");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookDirection = (ship.transform.position - transform.position).normalized;
+
+        asteroidRb.AddForce(lookDirection * speed);
+
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject)
+        {
+            Debug.Log("collision");
+            UpdateScore(2);
+            Destroy(gameObject);
+        }
+
+    }
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        ScoreText.text = "amogus:" + score;
+    }
+}
