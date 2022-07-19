4935943e08542d2d8ba7aafd4da3762f9889b873
caseynyu <lv2224@nyu.edu>
2022-07-18 21:40:33 -0400

    added interaction system and first weapon monitor

---
+++ Assets/Scripts/Interactables/WeaponsScreen.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponsScreen : Interactable
+{
+    [SerializeField]
+    private GameObject player;
+    [SerializeField]
+    private GameObject cam;
+    [SerializeField]
+    private Transform weaponsCameraPosition;
+    public float xMin;
+    public float xMax;
+    public float yMin;
+    public float yMax;
+    public bool isBeingUsed = false;
+    public Transform defaultCameraPosition;
+    public Transform defaultOrientation;
+
+    protected override void Interact()
+    {
+        if (isBeingUsed == true)
+        {
+            isBeingUsed = false;
+            player.GetComponent<PlayerMovementTutorial>().enabled = true;
+            player.GetComponentInChildren<MeshRenderer>().enabled = true;
+            MoveCamera moveCamera = cam.GetComponent<MoveCamera>();
+            moveCamera.cameraPosition = defaultCameraPosition;
+            moveCamera.orientation = defaultOrientation;
+            cam.GetComponent<Camera>().fieldOfView = 90;
+            moveCamera.xMin = -90;
+            moveCamera.xMax = 90;
+            moveCamera.yMin = -360;
+            moveCamera.yMax = 360;
+        }
+         else if (isBeingUsed == false)
+        {
+            isBeingUsed = true;
+            player.GetComponent<PlayerMovementTutorial>().enabled = false;
+            player.GetComponentInChildren<MeshRenderer>().enabled = false;
+            MoveCamera moveCamera = cam.GetComponent<MoveCamera>();
+            moveCamera.cameraPosition = weaponsCameraPosition;
+            cam.transform.rotation = weaponsCameraPosition.rotation;
+            moveCamera.orientation = weaponsCameraPosition;
+            cam.GetComponent<Camera>().fieldOfView = 60;
+            moveCamera.xMin = xMin;
+            moveCamera.xMax = xMax;
+            moveCamera.yMin = yMin;
+            moveCamera.yMax = yMax;
+        }
+        
+        
+        
+    }
+}
--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -11,10 +11,18 @@ public class MoveCamera : MonoBehaviour
     public Transform orientation;
     float xRotation;
     float yRotation;
+    public float xMin;
+    public float xMax;
+    public float yMin;
+    public float yMax;
 
     // Start is called before the first frame update
     void Start()
     {
+        xMin = -90;
+        xMax = 90;
+        yMax = 360;
+        yMin = -360;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
@@ -25,10 +33,10 @@ public class MoveCamera : MonoBehaviour
         transform.position = cameraPosition.position;
         float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
         float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
-
         yRotation += mouseX;
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90, 90);
+        xRotation = Mathf.Clamp(xRotation, xMin, xMax);
+        yRotation = Mathf.Clamp(yRotation, yMin, yMax);
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
         orientation.rotation = Quaternion.Euler(0, yRotation, 0);
     }
